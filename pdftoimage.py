cd74a13f16b386072cd06c9f77ad55b125896871
Tyler Miranda Hayashi <tylerymh27@gmail.com>
2023-05-12 10:56:32 -0400

    update

--- pdftoimage.py
+++ pdftoimage.py
@@ -7,23 +7,24 @@ pdf_folder = r"/Users/ty/Downloads/Fotos"
 savingFolder = r"/Users/ty/Downloads/FotosIMG"
 savingFolderRescale = r"/Users/ty/Downloads/FotosIMGRescale"
 
+Image.MAX_IMAGE_PIXELS = 1000000000
+
 for pdf_file in os.listdir(pdf_folder):
     if pdf_file.endswith(".pdf"):
         pdf_path = os.path.join(pdf_folder, pdf_file)
         pages = convert_from_path(pdf_path=pdf_path, poppler_path=poppler_path)
-
         saving_folder = savingFolder
         c = 1
 
         for page in pages:
             img_name = f"{pdf_file[:-4]}-img-{c}.jpg"
             image_path = os.path.join(saving_folder, img_name)
-            page.save(image_path, "JPEG", quality=50, optimize=True)
+            page.save(image_path, "JPEG", quality=70, optimize=True)
             # Load the image with PIL
             image = Image.open(image_path)
             # Check if the image is too big
-            max_width = 5000  # set your maximum width here
-            max_height = 5000  # set your maximum height here
+            max_width = 2000  # set your maximum width here
+            max_height = 2000  # set your maximum height here
             if image.width > max_width or image.height > max_height:
                 # Scale down the image while maintaining the aspect ratio
                 image.thumbnail((max_width, max_height), Image.ANTIALIAS)
